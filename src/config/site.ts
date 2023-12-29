67375a34a520ca57e33fde88d936b2f619aa8926
= <=>
2023-12-29 09:41:59 -0600

    create config and update btns

---
+++ src/config/site.ts
@@ -0,0 +1,9 @@
+
+const siteConfig = {
+    name: "Yash Bansod",
+    description: "Skilled Fullstck Develpoper",
+    url: "https://portfolio-two-xi-75.vercel.app",
+    gitHub : "https://github.com/yash-bansod-2003"
+}
+
+export { siteConfig }
