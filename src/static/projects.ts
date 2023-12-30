6ea279675501a2e83202a91098ebc97c3df4ce40
= <=>
2023-12-30 04:39:13 -0600

    implemented work section

---
+++ src/static/projects.ts
@@ -0,0 +1,62 @@
+interface Project {
+    name: string,
+    description: string,
+    image: string,
+    date: string,
+    gitHub: string,
+    preview?: string
+}
+
+
+export const data: Array<Project> = [
+    {
+        name: "Calculator",
+        description: "a advanced calculator built with cutting edge technologies and hav ea stunning features out of the box it will provide you a best experience ever so lts try it",
+        image: "/project/simpal_calculator.png",
+        date: "Sep 3, 2021",
+        gitHub: "",
+    },
+    {
+        name: "Postman Clone",
+        description: "a advanced calculator built with cutting edge technologies and hav ea stunning features out of the box it will provide you a best experience ever so lts try it",
+        image: "/project/postman.png",
+        date: "Sep 4, 2021",
+        gitHub: "",
+    },
+    {
+        name: "Responsive Analog Clock",
+        description: "a advanced calculator built with cutting edge technologies and hav ea stunning features out of the box it will provide you a best experience ever so lts try it",
+        image: "/project/clock.png",
+        date: "Sep 5, 2021",
+        gitHub: "",
+    },
+    {
+        name: "Run the word Game",
+        description: "a advanced calculator built with cutting edge technologies and hav ea stunning features out of the box it will provide you a best experience ever so lts try it",
+        image: "/project/run_game.png",
+        date: "Oct 23, 2021",
+        gitHub: "",
+    },
+    {
+        name: "Contact Manager",
+        description: "a advanced calculator built with cutting edge technologies and hav ea stunning features out of the box it will provide you a best experience ever so lts try it",
+        image: "/project/contactmanagementsystem.png",
+        date: "Jan 10, 2022",
+        gitHub: "",
+    },
+    {
+        name: "Portfolio Old",
+        description: "a advanced calculator built with cutting edge technologies and hav ea stunning features out of the box it will provide you a best experience ever so lts try it",
+        image: "/project/old_portfolio.png",
+        date: "Sep 3, 2023",
+        gitHub: "",
+    },
+    {
+        name: "Material Dashboard",
+        description: "a advanced calculator built with cutting edge technologies and hav ea stunning features out of the box it will provide you a best experience ever so lts try it",
+        image: "/project/_dashboard_mui.png",
+        date: "Feb 25, 2021",
+        gitHub: "",
+    },
+
+]
